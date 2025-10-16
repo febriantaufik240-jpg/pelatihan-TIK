5eab3e06e63cfede3993619049add74c2e27c76f
febriantaufik240-jpg <febriantaufik240@gmail.com>
2025-10-16 07:36:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const tombol sapa = documen.getElementById('sapaButton');
 //2. tambahkan 'event listener'untuk aksi 'clik'
 tombol sapa.addEventListener('clik', function(){
     //3.jalankan kode ini ketika tombol di-klik
-                               alert('Halo! Terima kasih sudah berkunjung!');
+     alert('Halo! Terima kasih sudah berkunjung!');
 });
