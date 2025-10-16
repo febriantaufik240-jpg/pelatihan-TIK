b8ea3fd560d06ae56891f34969f6ab3316654700
febriantaufik240-jpg <febriantaufik240@gmail.com>
2025-10-16 07:35:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasarkan ID-nya
+const tombol sapa = documen.getElementById('sapaButton');
+//2. tambahkan 'event listener'untuk aksi 'clik'
+tombol sapa.addEventListener('clik', function(){
+    //3.jalankan kode ini ketika tombol di-klik
+                               alert('Halo! Terima kasih sudah berkunjung!');
+});
